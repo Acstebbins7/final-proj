1078f7ec607861a29fb9ebff82f7ed452e3878f9
Adam Stebbins <acstebbins7@gmail.com>
2021-04-28 21:14:24 -0400

    Got the Photos the way I want them in Farm Life.

--- farmAPI.js
+++ farmAPI.js
@@ -1,7 +1,24 @@
 const fetch = require('node-fetch');
 
 (async () => {
-  const response = await fetch('https://trefle.io/api/v1/plants?token=rNcvKrCyAO_7NPzYh9ev56Q0UXSGul2ttWFRtMVfoOc');
-  const json = await response.json();
+  const response = fetch('https://trefle.io/api/v1/plants?token=rNcvKrCyAO_7NPzYh9ev56Q0UXSGul2ttWFRtMVfoOc');
+  const json = response.json();
   console.log(json);
 })();
+
+
+//  // Step 1: Make a GET request to the Dog API URL
+// fetch(URL)
+
+// // Step 2: Extract JSON from response
+// .then((res) => res.json())
+
+// // Step 3: Process JSON as we please
+// .then((data) => {
+
+//     console.log('success!');
+//     console.log(data.message);
+
+//     // TODO: Handle the data here
+
+// });
