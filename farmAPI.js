981e89ad965284cf40b2428e9e59f3db1ef814cd
Adam Stebbins <acstebbins7@gmail.com>
2021-04-29 22:23:08 -0400

    making more changes to the photos.

--- farmAPI.js
+++ farmAPI.js
@@ -38,17 +38,17 @@
 
 // Attempt #4
 
-getPlantData (); 
+// getPlantData (); 
 
-async function getPlantData() {
-    const response = await fetch('');
-    const data = await response.text();
+// async function getPlantData() {
+//     const response = await fetch('');
+//     const data = await response.text();
 
-    const table = data.split('\n').slice(1);
-    table.forEach(row => {
-        const columns = row.split(',');
-        const year = columns[0];
-        const temp = columns[1];
-        console.log(year, temp);
-    });
-}
+//     const table = data.split('\n').slice(1);
+//     table.forEach(row => {
+//         const columns = row.split(',');
+//         const year = columns[0];
+//         const temp = columns[1];
+//         console.log(year, temp);
+//     });
+// }
